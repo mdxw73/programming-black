064fa6f9051c75f4dbd3f729569deb0f4fcf30d6
Zack Obied <zack.obied@durham.ac.uk>
2021-12-12 19:49:43 +0000

    push

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const hostname = '127.0.0.1'
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    fs.readFile('index.html', function (err, data){
+    fs.readFile('poem.html', function (err, data){
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
         res.write(data);
